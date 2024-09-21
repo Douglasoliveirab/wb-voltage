c5a4b9b4bc2205b6f6c7ff4d8a5c3cd27f77d719
Douglasoliveirab <douglasoliveirabeserradev@gmail.com>
2024-09-21 15:51:31 -0300

    feat: primeiro commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Wbvoltage 
+
+Desenvolvimento de um para mostrar o trabalho da empresa Wbvoltage
+
+## Teccnologias 
+
+React + TypeScript + Vite + Tailwind
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className="flex justify-center w-full">
+      <p className='text-black'>wbvoltage em construção</p>
+
+    </div>
+  )
+}
+
+export default App
